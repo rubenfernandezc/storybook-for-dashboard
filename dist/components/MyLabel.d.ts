debe73795989609a3be9297202de961f70a9860c
rubenfernandezc <79019507+rubenfernandezc@users.noreply.github.com>
2022-09-11 12:03:13 +0200

    feat: add semantic-versioning

---
+++ dist/components/MyLabel.d.ts
@@ -0,0 +1,29 @@
+/// <reference types="react" />
+import './mylabel.css';
+interface MyLabelProps {
+    /**
+    * Label contents
+    */
+    label: string;
+    /**
+    * How large should the button be?
+    */
+    size: 'normal' | 'h1' | 'h2' | 'h3';
+    /**
+     * What color to use
+     * @default 'primary'
+     * */
+    color: 'primary' | 'secondary' | 'tertiary';
+    /**
+    * What font color to use
+    * @default 'primary'
+     * */
+    fontColor: string;
+    /**
+     * All caps
+     * @default false
+     * */
+    allCaps: boolean;
+}
+export declare const MyLabel: ({ label, size, color, allCaps, fontColor }: MyLabelProps) => JSX.Element;
+export {};
